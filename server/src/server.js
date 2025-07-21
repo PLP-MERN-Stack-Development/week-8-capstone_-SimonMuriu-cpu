19177720eea49916d3804779d38ca9d96aa2824c
Simon Muriu <muriusimon6@gmail.com>
2025-07-21 17:49:57 +0300

    Fix path to serve client build in Render

--- server/src/server.js
+++ server/src/server.js
@@ -78,11 +78,11 @@ app.use('/api/posts', postRoutes)
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   const clientDistPath = path.resolve(__dirname, '../client/dist')
-  app.use(express.static(clientDistPath))
+  app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(clientDistPath, 'index.html'))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "..", "client", "dist", "index.html"));
+  });
 }
 
 // Socket.IO connection handling
